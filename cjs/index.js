ac005543d518c7a43fb77e0ebcaba3c6f90eb2d2
Laxman <bytemode@outlook.com>
2020-07-04 11:40:29 +0530

    Add JSDoc annotations (#5)

--- cjs/index.js
+++ cjs/index.js
@@ -12,13 +12,40 @@ var _traverse = _interopRequireDefault(require("@babel/traverse"));
 
 var _generator = _interopRequireDefault(require("@babel/generator"));
 
+var rollup = _interopRequireWildcard(require("rollup"));
+
+function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return null; var cache = new WeakMap(); _getRequireWildcardCache = function () { return cache; }; return cache; }
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/**
+ * @param {string} code
+ * @returns {string}
+ */
+function defaultTransformer(code) {
+  return code;
+}
+/**
+ * @typedef {Object} TransformerOptions
+ * @property {ParserOptions} [parserOptions={}] - Parser options for Babel
+ * @property {string[]} [tagsToProcess=[]] - List of named template tags to process
+ * @property {defaultTransformer} [transformer] - Callback function for handling piece of code
+ */
+
+/**
+ * @param {string} content
+ * @param {TransformerOptions} [options={}]
+ * @returns {string}
+ */
+
 
 function transformTaggedContent(content, options = {}) {
   const {
     parserOptions = {},
     tagsToProcess = [],
-    transformer = code => code
+    transformer = defaultTransformer
   } = options;
   const ast = (0, _parser.parse)(content, parserOptions);
   (0, _traverse.default)(ast, {
@@ -35,8 +62,13 @@ function transformTaggedContent(content, options = {}) {
   });
   return (0, _generator.default)(ast);
 }
+/**
+ * @type {rollup.Plugin}
+ * @param {TransformerOptions} [options={}]
+ */
 
-function transformTaggedTemplate(options) {
+
+function transformTaggedTemplate(options = {}) {
   return {
     name: 'transform-tagged-template',
 
--- esm/index.js
+++ esm/index.js
@@ -1,12 +1,33 @@
-import {parse} from '@babel/parser';
+import {parse, ParserOptions} from '@babel/parser';
 import traverse from '@babel/traverse';
 import generate from '@babel/generator';
+import * as rollup from 'rollup';
 
+/**
+ * @param {string} code
+ * @returns {string}
+ */
+function defaultTransformer(code) {
+	return code;
+}
+
+/**
+ * @typedef {Object} TransformerOptions
+ * @property {ParserOptions} [parserOptions={}] - Parser options for Babel
+ * @property {string[]} [tagsToProcess=[]] - List of named template tags to process
+ * @property {defaultTransformer} [transformer] - Callback function for handling piece of code
+ */
+
+/**
+ * @param {string} content
+ * @param {TransformerOptions} [options={}]
+ * @returns {string}
+ */
 export function transformTaggedContent(content, options = {}) {
 	const {
 		parserOptions = {},
 		tagsToProcess = [],
-		transformer = code => code
+		transformer = defaultTransformer
 	} = options;
 
 	const ast = parse(content, parserOptions);
@@ -26,7 +47,11 @@ export function transformTaggedContent(content, options = {}) {
 	return generate(ast);
 }
 
-export default function transformTaggedTemplate(options) {
+/**
+ * @type {rollup.Plugin}
+ * @param {TransformerOptions} [options={}]
+ */
+export default function transformTaggedTemplate(options = {}) {
 	return {
 		name: 'transform-tagged-template',
 		transform(content) {
